20b324fc0bb2dbe10cb01e7cb111f1960c57a3f3
madelen-illanes <madelen2811@gmail.com>
2022-09-30 20:35:46 -0500

    modificacion

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,14 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-<<<<<<< HEAD
 import { PlayersService } from 'src/app/_services/player/players.service';
 import { Player } from '../../_models/players';
-=======
-import {CardService} from "../services/card.service";
-import {Player} from "../../interface/player.interface";
-import {Observable,} from "rxjs";
-import {PositionPlayer} from "../../interface/position.interface";
->>>>>>> 512be9b4932fb5b69ec8836036df9f7e96413a08
 
 
 @Component({
@@ -27,27 +20,9 @@ export class SliderComponent implements ControlValueAccessor {
   @Input()
   public label: string = '';
   field = '0';
-<<<<<<< HEAD
   players: Player[] = [];
   nombre!: string;
 
-=======
-  players : Player[] = [];
-  positionPlayer : PositionPlayer [] =[];
-  players$:Observable<Player[]>
-
-
-
-  constructor(
-      private cardService: CardService,
-
-  ) {
-    {
-      this.players$ = this.cardService.getPlayers()
-    }
-
-  }
->>>>>>> 512be9b4932fb5b69ec8836036df9f7e96413a08
 
   constructor(private playerServices: PlayersService) {}
 
@@ -91,50 +66,6 @@ export class SliderComponent implements ControlValueAccessor {
     this.onTouch = onTouched;
   }
 
-<<<<<<< HEAD
   
-=======
-
-  ngOnInit(): void {
-   // this.activateRoute.params
-    this.getPlayers();
-        //
-        // .pipe(
-        //     switchMap(({id})=> this.cardService.getPlayer(id))
-        // ).subscribe(resp => {console.log(resp);this.players = resp});
-
-  }
-  getPlayers() {
-    this.cardService.getPlayers()
-        .subscribe(players => {
-          this.players = players;
-        })
-  }
-
-  // get bookTitle() {
-  //   return this.players?.firstName
-  //   // return (this.book && this.book.title) ? this.book.title : null
-  // }
-  // get bookId(){
-  //   return this.book?.id
-  // }
-  //
-  // get bookImage() {
-  //   return this.lastName?.image
-  // }
-  //
-  // get bookAuthor() {
-  //   return this.attack?.author
-  // }
-  //
-  // get bookUrl() {
-  //   return this.defense?.url
-  // }
-  //
-  // get bookResume() {
-  //   return this.skills?.resume
-  // }
-
->>>>>>> 512be9b4932fb5b69ec8836036df9f7e96413a08
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,7 @@
 
 export const environment = {
   production: false,
-<<<<<<< HEAD
   URL: 'https://api-exercise-q3.herokuapp.com'
-=======
-  api: 'https://api-exercise-q3.herokuapp.com'
->>>>>>> 512be9b4932fb5b69ec8836036df9f7e96413a08
 };
 
 /*
